a4ca1fedcce070b1c79238d3212d4a36d838c86b
daidensacha <daidensacha@gmail.com>
2022-08-09 20:23:05 +0200

    Add form handling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from '../styles/contact.module.scss';
 import AnimatedPage from './AnimatedPage';
 import cx from 'classnames';
@@ -6,16 +7,47 @@ import cx from 'classnames';
 // Import the NavLink component.
 
 const Contact = () => {
+  const [inputValues, setInputValues] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+  // Handle input change.
+  const handleChange = e => {
+    e.persist();
+    setInputValues({ ...inputValues, [e.target.name]: e.target.value });
+  };
+  // Handle form submission.
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(inputValues);
+    const { name, email, subject, message } = inputValues;
+
+    alert(
+      !name || !email || !subject || !message
+        ? `Missing field in your form.`
+        : `Thank you ${name} for submitting your data!`,
+    );
+    e.target.name.value = '';
+    e.target.email.value = '';
+    e.target.subject.value = '';
+    e.target.message.value = '';
+    setInputValues({ name: '', email: '', subject: '', message: '' });
+  };
+
   return (
     <AnimatedPage>
       <div className={styles.main}>
         {/* <h1 className={styles.h1}>Contact</h1> */}
-        <form className={styles.contactForm}>
-        <h1 className={styles.h1}>Contact</h1>
+        <form className={styles.contactForm} onSubmit={e => handleSubmit(e)}>
+          <h1 className={styles.h1}>Contact</h1>
           <div className={styles.formGroup}>
             <input
               type='text'
               name='name'
+              onChange={e => handleChange(e)}
+              value={inputValues.name}
               className={styles.formControl}
               id='name'
               placeholder='Enter your name'
@@ -24,6 +56,8 @@ const Contact = () => {
             <input
               type='email'
               name='email'
+              onChange={e => handleChange(e)}
+              value={inputValues.email}
               className={styles.formControl}
               id='email'
               placeholder='Enter your email'
@@ -32,6 +66,8 @@ const Contact = () => {
             <input
               type='text'
               name='subject'
+              onChange={e => handleChange(e)}
+              value={inputValues.subject}
               className={styles.formControl}
               id='subject'
               placeholder='Enter your subject'
@@ -41,6 +77,8 @@ const Contact = () => {
               className={styles.formControl}
               name='message'
               id='message'
+              onChange={e => handleChange(e)}
+              value={inputValues.message}
               rows='3'
               placeholder='Enter your message'></textarea>
             <button
