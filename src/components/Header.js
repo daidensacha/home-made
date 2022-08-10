7c7f131915c20d499578ccf8694c2adecbe1ac28
daidensacha <daidensacha@gmail.com>
2022-08-10 22:38:40 +0200

    Add animation transition to search results

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,64 +1,83 @@
 import React, { useState } from 'react';
 import '../styles/header.scss';
-// import logo from '../images/logo.png';
-// import { useDispatch, useSelector } from 'react-redux';
+
 import { BsSearch } from 'react-icons/bs';
 
 // Import the NavLink component.
 import { NavLink } from 'react-router-dom';
 import cx from 'classnames';
 
-const Header = ({articles}) => {
-
+const Header = ({ articles }) => {
   // Set state for search query
   const [query, setQuery] = useState('');
 
+  const [toggleShow, setToggleShow] = useState({
+    show: false,
+    class: '',
+  });
+
+  // Handle search query and set toggle show to true or false
   const handleChange = event => {
     let lowerCaseQuery = event.target.value.toLowerCase();
     setQuery(lowerCaseQuery);
-  };
-  // console.log(query);
-  // console.log("header", articles[0].title.toLowerCase());
-  const filteredData = articles.filter((article) => {
-    if (!query) {
-      return ''
+    if (lowerCaseQuery.length > 0) {
+      setToggleShow({
+        show: true,
+        class: 'show',
+      });
     } else {
-    return article.title.toLowerCase().includes(query);
+      setToggleShow({
+        show: false,
+        class: '',
+      });
     }
-  })
-  console.log("filteredData", filteredData);
+  };
+  //Filter articles by search query
+  const filteredData = articles.filter(article => {
+    if (!query) {
+      return '';
+    } else {
+      return article.title.toLowerCase().includes(query);
+    }
+  });
+  console.log('filteredData', filteredData.length);
 
+  // On click of link reset toggleShow to empty seach input and hide search results
+  const clickHandler = e => {
+    setQuery('');
+    setToggleShow({
+      show: false,
+      class: '',
+    });
+  };
 
   return (
     <div className='header'>
-
-{ query && (
-       <div className='queryContainer'>
+      <div className={cx(`queryContainer ${toggleShow.class}`)}>
         <div className='searchResults'>
-          <h3>Search results for "{query}" results:</h3>
-          {filteredData.map((article) => {
-            return (
-                 <div key={article.id}>
-                <NavLink
-                className="articleLink"
-                to={`/article/${article.slug}`}
-                onClick={() => setQuery('')}
-                >
-                  {article.title}
+          {filteredData.length ? (
+            <h3>Search results for "{query}"</h3>
+          ) : (
+            <h3>No results for search "{query}"</h3>
+          )}
 
+          {filteredData.map(article => {
+            return (
+              <div key={article.id}>
+                <NavLink
+                  className='articleLink'
+                  to={`/article/${article.slug}`}
+                  onClick={clickHandler}>
+                  {article.title}
                 </NavLink>
               </div>
-              )
-            })
-          }
+            );
+          })}
         </div>
       </div>
-)}
       <div className='logoWrapper'>
-        <NavLink
-        className="logoLink"
-        to='/'>
-        <h3>Home Made</h3>
+        <NavLink className='logoLink' to='/'>
+          <h3>Home Made</h3>
         </NavLink>
       </div>
 
