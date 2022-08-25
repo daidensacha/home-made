5c1bc0466c5fb50b6078a1545aafdddbeb6416cc
daidensacha <daidensacha@gmail.com>
2022-08-25 08:07:45 +0200

    Add parseInt to id from useParams so it passes strict comparison

--- src/components/Author.js
+++ src/components/Author.js
@@ -7,43 +7,41 @@ import styles from '../styles/author.module.scss';
 
 const Author = ({ articles, authors }) => {
   const { author_id } = useParams();
-
+  console.log('author_id', typeof author_id);
   const author = authors?.find(author => author.id === parseInt(author_id));
-  console.log('author', author);
+  // const author = authors?.find(author => author.id == author_id);
+  // console.log('author', author);
   // console.log('authors: ', authors);
 
   return (
     <AnimatedPage>
-      {author && (<div className={styles.main}>
-        <div className={styles.authorBio}>
-          <h1 className={styles.h1}>Author Bio</h1>
-          <div className={styles.container}>
-            <div className={styles.bioImage}>
-              <img
-                src={author?.image_url}
-                alt={author?.image_title}
-              />
-            </div>
-            <div className={styles.bioContent}>
-              <h3 className={styles.h3}>{author?.name}</h3>
-              {/* <Markdown>{author?.bio}</Markdown> */}
-              <p>{author?.bio}</p>
-              <div className={styles.authorBtns}>
-              <Link
-                className={styles.btn}
-                to={`/articles/authorArticles/${author?.id}`}>
-                Authors Articles
-              </Link>
-              <Link
-                className={styles.btn}
-                to={`/articles/`}>
-                All Articles
-              </Link>
+      {author && (
+        <div className={styles.main}>
+          <div className={styles.authorBio}>
+            <h1 className={styles.h1}>Author Bio</h1>
+            <div className={styles.container}>
+              <div className={styles.bioImage}>
+                <img src={author?.image_url} alt={author?.image_title} />
+              </div>
+              <div className={styles.bioContent}>
+                <h3 className={styles.h3}>{author?.name}</h3>
+                <Markdown>{author?.bio}</Markdown>
+                {/* <p>{author?.bio}</p> */}
+                <div className={styles.authorBtns}>
+                  <Link
+                    className={styles.btn}
+                    to={`/articles/authorArticles/${author?.id}`}>
+                    Authors Articles
+                  </Link>
+                  <Link className={styles.btn} to={`/articles/`}>
+                    All Articles
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>)}
+      )}
     </AnimatedPage>
   );
 };
